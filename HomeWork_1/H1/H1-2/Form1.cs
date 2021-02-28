8c5e8fe894d068339098a47258f4d6edb1b17aef
itzdj <hello@itzdj.cn>
2021-02-28 21:17:50 +0800

    rebuild again

--- HomeWork_1/H1/H1-2/Form1.cs
+++ HomeWork_1/H1/H1-2/Form1.cs
@@ -8,34 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace hw1_2
+namespace H1_2
 {
-    //partial表示同一个类放在不同的文件中
     public partial class Form1 : Form
     {
-
         static double num1;
         static double num2;
         static string symbol;
-
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            string n1 = textBox1.Text;
-            num1 = InputNum(n1);
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            string n2 = textBox2.Text;
-            num2 = InputNum(n2);
-
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -67,27 +51,36 @@ namespace hw1_2
             catch { return false; }
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {       
-            symbol = comboBox1.Text;
         }
 
-
-        private void button1_Click(object sender, EventArgs e)
-        {                   
+        private void button1_Click_1(object sender, EventArgs e)
+        {
             if (symbol == "+") { MessageBox.Show("答案是：" + (num1 + num2)); }
             if (symbol == "-") { MessageBox.Show("答案是：" + (num1 - num2)); }
             if (symbol == "*") { MessageBox.Show("答案是：" + (num1 * num2)); }
             if (symbol == "/" && num2 != 0) { MessageBox.Show("答案是：" + (num1 / num2)); }
             if (symbol == "%") { MessageBox.Show("答案是：" + (num1 % num2)); }
-            else { MessageBox.Show("请选择正确的操作数和符号!"); }
-            
+            else { MessageBox.Show("There are something wrong!", "ERROR"); }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
+            symbol = comboBox1.Text;
+        }
 
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            string n1 = textBox1.Text;
+            num1 = InputNum(n1);
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            string n2 = textBox2.Text;
+            num2 = InputNum(n2);
         }
     }
 }
