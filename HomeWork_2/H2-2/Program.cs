c02d6f6e3e38ca0d1a338806e9430673156a0f65
itzdj <hello@itzdj.cn>
2021-03-04 22:06:12 +0800

    h2部分题目和h1目录结构

---
+++ HomeWork_2/H2-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_2
+{
+    class Program
+    {
+        static int min=int.MaxValue;
+        static int max=int.MinValue;
+        static void Main(string[] args)
+        {
+            int[] ints = new int[] { 1, 16, 15, 3, 7, 8, 9, 5, 5, 6, 3, 2, 17 };
+            FindMaxAndMin(ints, ref min,ref max);
+            int sum = Sum(ints);
+            Console.WriteLine("sum="+sum+" ,min="+min+" ,max="+max);
+
+        }
+
+        static void FindMaxAndMin(int[] ints, ref int min,ref int max) {
+            for (int i = 0; i < ints.Length; i++) {
+                if (ints[i] < min) { min = ints[i]; }
+                if (ints[i] > max) { max = ints[i]; }
+            }
+        }
+
+        static int Sum(int[] ints) {
+            int sum = 0;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                sum += ints[i];           
+            }
+            return sum;
+        }
+    }
+}
---
+++ HomeWork_2/H2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2
+{
+    class Program
+    {
+        static int num;
+        static string str;
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            while (true) {
+                try { 
+                    string str = Console.ReadLine();
+                    num = int.Parse(str);
+                    break;
+                }
+                catch { Console.WriteLine("输入不合法，请重新输入："); }
+            }
+
+             str = num + "=";
+            FindPrimeFactor();
+            if (count == 0) { Console.WriteLine(num); }
+            else { Console.WriteLine(str); }
+            
+        }
+
+        static void FindPrimeFactor()
+        {
+            int max = num;
+            for (int i = 2; i < Math.Sqrt(max); i++)
+            {
+                while (true) {
+                    if (num % i == 0)
+                    {
+                        count++;//标记输入的数据不是素数
+                        str += i;
+                        num /= i;
+                        if (num != 1)
+                        {
+                            str += "*";
+                        }
+                    }
+                    else { break; }
+                    
+                }  
+            }
+            if (num != 1)
+            {
+                str +=num;
+            }
+        }
+
+
+    }
+    
+}
