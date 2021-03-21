9bb08cc3505af8afaba4df3b4b0b8f9a9ef91023
itzdj <hello@itzdj.cn>
2021-03-21 13:12:39 +0800

    h4

---
+++ HomeWork_4/H4-2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace H4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockExt clx = new ClockExt();
+            clx.myclock.SetRingTime(3);//模拟设置5秒后响铃
+            clx.myclock.StartClock();//模拟开启闹钟
+        }
+   
+    }
+
+    public delegate void ClockHandler(object sender, AlarmEventArgs args);
+   
+    public class AlarmEventArgs{}
+
+  
+    
+    //闹钟类
+    public class Clock {
+        public event ClockHandler tick;
+        public event ClockHandler ring;//定义两个事件
+
+        int ringtime=0;
+
+
+        //开启闹钟
+        public void StartClock() {
+            Console.WriteLine("闹钟打开了：");
+            AlarmEventArgs args = new AlarmEventArgs(); 
+            while (ringtime > 0)
+            {
+                tick(this, args);//触发滴答事件
+                System.Threading.Thread.Sleep(1000);//当前主线程sleep
+                ringtime--;
+            }
+            Ring();
+            while (true)
+            {
+                tick(this, args);//触发滴答事件    
+                System.Threading.Thread.Sleep(1000);//当前主线程sleep
+            }
+        }
+
+        public void SetRingTime(int num)
+        {
+            if (num <= 0) { throw new ArgumentOutOfRangeException("不合理的时间"); }
+            else { Console.WriteLine("已经设置闹钟的值..."); }
+            ringtime = num;            
+        }
+
+        public void Ring() {
+            AlarmEventArgs args = new AlarmEventArgs();
+            ring(this, args);//触发响铃事件
+        }
+
+
+
+
+
+
+    }
+
+
+    //闹钟扩展类，类比按钮对应的Form类
+    public class ClockExt {
+        public Clock myclock = new Clock();
+
+        public ClockExt() {
+            //把一个委托赋值给另一个 new ClockHandler(
+            myclock.tick += ClockTick;
+            myclock.ring += ClockRing;
+        }
+
+        void ClockTick(object sender, AlarmEventArgs args) {
+                Console.WriteLine("tick......当前系统时间是：{0}", DateTime.Now);//当前系统时间        
+        }
+
+        void ClockRing(object sender, AlarmEventArgs args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("ddddd.......闹钟响了");
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ HomeWork_4/H4/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4
+{
+    //主类
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints={2,3,5,6,6,6,9,6,3,1,2,8,7};
+            GenericList<int> list= new GenericList<int>();
+            foreach (int var in ints) {
+                list.Add(var);
+            }
+            Node<int> head=list.Head;
+            
+            list.ForEach(head ,temp=>Console.Write(temp + " "));         
+            Console.WriteLine();
+
+            int max = int.MinValue;
+            list.ForEach(head, temp => max = Math.Max(temp, max));
+            Console.WriteLine("max= " + max);
+
+            int min = int.MaxValue;
+            list.ForEach(head, temp => min = Math.Min(temp, min));
+            Console.WriteLine("min= " + min);
+
+            int sum = -1;
+            list.ForEach(head, temp => sum += temp);
+            Console.WriteLine("sum= " + sum);
+        }
+    }
+
+
+    //结点类
+    public class Node<T> { 
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }   
+    }
+
+
+    //泛型链表类
+    public class GenericList<T> {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList() {
+            head = tail = null;
+        }
+
+
+        public Node<T> Head{
+        get=>head;
+        }
+
+        public void Add(T t){
+        Node<T> n=new Node<T>(t);
+            if(tail==null){
+            head=tail=n;
+            }else{
+            tail.Next=n;
+            tail=n;
+            }
+        }
+
+        public void ForEach(Node<T> n,Action<T> action)
+        {
+            Node<T> node=n;
+            while(node!=null){
+                action(node.Data);//调用函数操作结点数据
+                node=node.Next;
+            }
+        }
+   
+    }
+
+
+
+}
